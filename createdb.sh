dffd9bde82d200e020cf3df4b4a82fc774cd1a56
asc <asc@cloud.spum.org>
2010-07-28 23:46:09 +0000

    initial commit

---
+++ createdb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then
+        echo "Usage: $0 <user name> <database name>";
+        exit;
+fi;
+
+USERNAME=$1
+DATABASE=$2
+
+createuser --no-superuser --no-createdb --no-createrole $USERNAME;
+createdb --owner=$USERNAME $DATABASE;
+createlang plpgsql $DATABASE;
+
+psql -f /usr/share/postgresql-8.3-postgis/lwpostgis.sql $DATABASE;
+psql -f /usr/share/postgresql-8.3-postgis/spatial_ref_sys.sql $DATABASE;
+psql -f 900913.def $DATABASE;
+
+echo "ALTER TABLE geometry_columns OWNER TO $USERNAME;" | psql $DATABASE;
+echo "ALTER TABLE spatial_ref_sys OWNER TO $USERNAME;" | psql $DATABASE;
