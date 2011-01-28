9f746b93ffff2c67a8f9bf350cd6c3cac3a59b46
straup <straup@gmail.com>
2011-01-28 17:48:49 +0100

    removed non-prefixed files; update scripts accordingly

--- createdb-8.3.sh
+++ createdb-8.3.sh
@@ -12,9 +12,11 @@ createuser --no-superuser --no-createdb --no-createrole $USERNAME;
 createdb --owner=$USERNAME $DATABASE;
 createlang plpgsql $DATABASE;
 
-psql -f lwpostgis.sql $DATABASE;
-psql -f spatial_ref_sys.sql $DATABASE;
-psql -f 900913.def $DATABASE;
+psql -f lwpostgis-8.3.sql $DATABASE;
+psql -f spatial_ref_sys-8.3.sql $DATABASE;
+psql -f spatial_ref_900913-8.3.sql $DATABASE;
+
+# TO DO: find the 8.3 _int.sql and include it here
 
 echo "ALTER TABLE geometry_columns OWNER TO $USERNAME;" | psql $DATABASE;
 echo "ALTER TABLE spatial_ref_sys OWNER TO $USERNAME;" | psql $DATABASE;
